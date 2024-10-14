1a1f56c4ec3d8d015f87d8ec8d157f59c557ee56
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-10-14 11:26:17 +0300

    Update process_stream

--- stream_pulse/src/lib/process_stream.rs
+++ stream_pulse/src/lib/process_stream.rs
@@ -1,4 +1,4 @@
-use std::{path::PathBuf, sync::LazyLock};
+use std::{fs::create_dir_all, path::PathBuf, sync::LazyLock};
 
 use anyhow::Context;
 use rayon::prelude::*;
@@ -7,12 +7,7 @@ use ytdlp_bindings::{AudioProcessor, YtDlp, YtDlpError};
 
 use crate::{extract_json_from_script, parse_streams};
 
-static CLIENT: LazyLock<Result<reqwest::Client, reqwest::Error>> = LazyLock::new(|| {
-    reqwest::Client::builder()
-    // Because sometimes, even bots want to feel like humans.
-    .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
-    .build()
-});
+static CLIENT: LazyLock<reqwest::Client> = LazyLock::new(reqwest::Client::new);
 
 static YTDLP: LazyLock<Result<YtDlp, YtDlpError>> = LazyLock::new(YtDlp::new);
 
@@ -24,7 +19,7 @@ static OPENAI: LazyLock<openai_dive::v1::api::Client> =
 const YOUTUBE_STREAM_URL: &str = "https://www.youtube.com/@ParliamentofKenyaChannel/streams";
 
 pub async fn fetch_and_process_streams() -> anyhow::Result<()> {
-    let client = CLIENT.as_ref()?;
+    let client = &CLIENT;
     let ytdlp = YTDLP.as_ref()?;
 
     let db = DataStore::new("bunge-bits-store.db").context("Failed to connect to database")?;
@@ -54,10 +49,8 @@ pub async fn fetch_and_process_streams() -> anyhow::Result<()> {
                 let mut audio_out_path = audio_download_path.join(&stream.video_id);
                 let vtt_output_path = autosub_path.join(&stream.video_id);
                 // This is the directory we store the chunked audio files
-                let chunked_audio_path = PathBuf::from(format!(
-                    "/var/tmp/bunge-bits/chunked-audio/{}",
-                    stream.video_id
-                ));
+                let chunked_audio_path =
+                    PathBuf::from(format!("/var/tmp/bunge-bits/audio/{}", stream.video_id));
 
                 // Download autosub
                 ytdlp.download_auto_sub(&youtube_stream, &vtt_output_path)?;
@@ -68,11 +61,14 @@ pub async fn fetch_and_process_streams() -> anyhow::Result<()> {
                 // set mp3 extension
                 audio_out_path.set_extension("mp3");
 
+                // create nested `/stream.id/` dir
+                create_dir_all(&chunked_audio_path).expect("Failed to create directories");
+
                 // Split downloaded audio to chunks
                 ytdlp.split_audio_to_chunks(
                     audio_out_path,
                     1800,
-                    chunked_audio_path.join("file_%03d.mp3"),
+                    chunked_audio_path.join(format!("{}_%03d.mp3", stream.video_id)),
                 )?;
 
                 Ok::<_, anyhow::Error>(())
