98cb303fd3102f2c6d46ee1fad0a4f3cf065c307
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-06-16 16:26:00 +0300

    Fix fmt

--- crates/stream_pulse/src/lib/process_stream.rs
+++ crates/stream_pulse/src/lib/process_stream.rs
@@ -76,7 +76,6 @@ pub async fn fetch_and_process_streams() -> anyhow::Result<()> {
             summarize_streams(&mut streams, Arc::new(OPENAI.clone()), &db).await?;
 
             // TODO: Generate timestamp_md
-
         }
         Err(e) => {
             tracing::error!(error = ?e,  "Error extracing ytInitialData from the html document");
