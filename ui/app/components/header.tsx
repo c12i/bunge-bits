d952823c526aa63c4ef967971d4a69819b5debd4
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-07-03 17:26:28 +0300

    chore: Lint fix

--- ui/app/components/header.tsx
+++ ui/app/components/header.tsx
@@ -1,6 +1,6 @@
+import { Link } from "@remix-run/react";
 import { X } from "lucide-react";
 import { useEffect, useState } from "react";
-import { Link } from "@remix-run/react";
 
 const DONATION_BANNER_KEY = "hideDonationBanner";
 
