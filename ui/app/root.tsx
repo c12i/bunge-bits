ccfef7dbee4be2f41f620acfc32d0768b011dd39
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-07-21 19:07:22 +0300

    chore: stick to kebab-case for pathnames

--- ui/app/root.tsx
+++ ui/app/root.tsx
@@ -14,7 +14,7 @@ import {
 import ErrorPage from "./components/error-page";
 import Footer from "./components/footer";
 import Header from "./components/header";
-import { NavigationProgress } from "./components/NavigationProgress";
+import { NavigationProgress } from "./components/navigation-progress";
 
 export const links: LinksFunction = () => [
   {
